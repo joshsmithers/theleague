3eebe6993b9d1b894c0ab9ac381b3c08cca77400
joshsmithers <jsmithers587@gmail.com>
2022-12-26 20:15:57 -0500

    shitty landing page

---
+++ src/components/TempLanding.vue
@@ -0,0 +1,55 @@
+<template>
+  <body>
+    <h2>
+    Welcome
+    </h2>
+
+  <div class="column">
+    <p>
+    to the future home of
+    </p>
+
+    <img class="icon" src="../assets/theLeague.jpg">
+  </div>
+  </body>
+  <div class="footer">
+    THE LEAGUE | EST. 2013
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TempLanding.vue"
+}
+</script>
+
+<style scoped>
+
+h2 {
+  align-self: flex-start;
+  padding: 2rem 2rem;
+  font-size: 3rem;
+}
+
+.column {
+  align-self: center;
+  display: flex;
+  flex-direction: column;
+
+}
+
+.column p {
+  margin: 0 0 5rem 0;
+  font-size: 1.5rem;
+}
+
+.icon {
+  width: 50%;
+  align-self: auto;
+}
+
+.footer {
+  display: flex;
+  margin-top: auto;
+}
+</style>
